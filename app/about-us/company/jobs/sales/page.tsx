734fa723a8b2997969651ade64e6ddaabdaf354a
julia2524 <61957239+julia2524@users.noreply.github.com>
2026-04-29 12:42:58 +0900

    about us writing

--- app/about-us/company/jobs/sales/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "About us",
-};
-
-export default function Sales() {
-  return <h1>Sales jobs</h1>;
-}
--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,11 +1,25 @@
+import styles from "../../styles/about-us.module.css";
+
 export const metadata = {
   title: "About Us",
 };
 
 export default function AboutUs() {
   return (
-    <div>
-      <h1>About Us!</h1>
+    <div className={styles.container}>
+      <p>Welcome to the Movie App!</p>
+      <p>
+        This web application allows users to explore movies, watch trailers,
+        check ratings, and discover similar films.
+      </p>
+
+      <p>
+        It was built using Next.js and the TMDB API as part of a learning
+        project to practice modern frontend development.
+      </p>
+
+      <p>Movie data is provided by TMDB.</p>
+      <p>&copy; 2026 Song. Built with Next.js and TMDB API.</p>
     </div>
   );
 }
